1d74524f110226f9a5f173042ab8fad65c5363c1
Lorenzo Rota <lorenzokaide@gmail.com>
2025-02-26 14:46:58 +0100

    Question 1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,52 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.types.DataTypes;
+import org.apache.spark.sql.types.StructField;
+import org.apache.spark.sql.types.StructType;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        // Disable logging
+        Logger.getLogger("org").setLevel(Level.ERROR);
+        Logger.getLogger("akka").setLevel(Level.ERROR);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        SparkConf conf = new SparkConf()
+                .setAppName("YfitopS Analytics")
+                .setMaster("local[*]");
+        JavaSparkContext sc = new JavaSparkContext(conf);
+
+        String filePath = "src/main/resources/plays.csv";
+
+        //1a
+        SparkSession spark = SparkSession
+                .builder()
+                .appName("YfitopS Analytics")
+                .master("local[*]")
+                .getOrCreate();
+
+        StructType schema = DataTypes.createStructType(new StructField[] {
+                DataTypes.createStructField("userId", DataTypes.IntegerType, false),
+                DataTypes.createStructField("songId", DataTypes.IntegerType, false),
+                DataTypes.createStructField("rating", DataTypes.IntegerType, true)  // rating is nullable
+        });
+
+        Dataset<Row> playsDF = spark.read()
+                .option("header", "false")
+                .option("inferSchema", "false")
+                .schema(schema)
+                .csv(filePath);
+
+        //1b
+        JavaRDD<String> playsRDD = sc.textFile(filePath);
+
+        sc.stop();
     }
 }
