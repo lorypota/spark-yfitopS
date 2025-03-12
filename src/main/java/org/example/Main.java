6592a8226a3ce461e59d102a3f81747853339392
Lorenzo Rota <lorenzokaide@gmail.com>
2025-03-12 14:41:27 +0100

    Question 2 fix + Question 3

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -14,6 +14,7 @@ import org.apache.spark.sql.types.StructField;
 import org.apache.spark.sql.types.StructType;
 import scala.Tuple2;
 
+import java.util.Comparator;
 import java.util.List;
 
 public class Main {
@@ -49,10 +50,10 @@ public class Main {
                 .csv(filePath);
 
         //1b
-        JavaRDD<String> playsRDD = sc.textFile(filePath);
+        JavaRDD<String> plays = sc.textFile(filePath);
 
         //2
-        JavaRDD<Integer[]> userWithRatingRDD = playsRDD
+        JavaRDD<Integer[]> userWithRating = plays
                 .filter(line -> {
                     String[] parts = line.split(",");
                     return parts.length == 3;
@@ -62,22 +63,35 @@ public class Main {
                     return new Integer[] {Integer.parseInt(parts[0]), Integer.parseInt(parts[2])};
                 });
 
-        JavaPairRDD<Integer, Tuple2<Integer, Integer>> userWithRatingCountRDD = userWithRatingRDD
+        JavaPairRDD<Integer, Tuple2<Integer, Integer>> userWithRatingCount = userWithRating
                 .mapToPair(arr -> new Tuple2<>(arr[0], new Tuple2<>(arr[1], 1)))
                 .reduceByKey((tuple1, tuple2) ->
                     new Tuple2<>(tuple1._1 + tuple2._1, tuple1._2 + tuple2._2)
                 );
 
-        JavaPairRDD<Integer, Tuple2<Integer, Integer>> tenRatingsOrMoreRDD = userWithRatingCountRDD
+        JavaPairRDD<Integer, Tuple2<Integer, Integer>> tenRatingsOrMore = userWithRatingCount
                 .filter(tuple -> tuple._2._2>=10);
 
-        JavaPairRDD<Integer, Double> avgRatingRDD = tenRatingsOrMoreRDD
+        JavaPairRDD<Integer, Double> avgRating = tenRatingsOrMore
                 .mapValues(tuple -> (double) tuple._1/tuple._2);
 
-        Tuple2<Integer, Double> topUser = avgRatingRDD
-                .reduce((tuple1, tuple2) -> tuple1._2 > tuple2._2 ? tuple1 : tuple2);
+        double maxAvgRating = avgRating
+                .map(tuple -> tuple._2)
+                .reduce(Math::max);
 
-        System.out.println(topUser);
+        JavaPairRDD<Integer, Double> topUsers = avgRating
+                .filter(tuple -> tuple._2.equals(maxAvgRating));
+
+        //3
+        int minUserId = topUsers
+                .keys()
+                .min(Comparator.naturalOrder());
+
+        JavaPairRDD<Integer, Double> topUserWithSmallestId = topUsers
+                .filter(tuple -> tuple._1.equals(minUserId));
+
+        topUserWithSmallestId.foreach(tuple ->
+                System.out.println("User ID: " + tuple._1 + ", Average Rating: " + tuple._2));
 
         sc.stop();
     }
